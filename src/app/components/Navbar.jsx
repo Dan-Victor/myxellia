1624a8d3718166336ffb187b89c6192defa49eba
Dan-Victor <victoradesola8@gmail.com>
2025-08-18 10:04:53 +0100

    updated

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     return (
     <>
     <header className="  w-full bg-black h-auto flex items-center justify-between px-[78px] py-[20px] z-999 fixed">
-     <img src="../Myxellia Logo.png" alt="Myxellia Logo"  className="" />
+     <img src="./MyxelliaLogo.png" alt="Myxellia Logo"  className="" />
      <nav className="flex w-auto items-center justify-center gap-[25px]">
-     <img src="../Notification 1.png" alt="Myxellia Logo" className="" />
-     <img src="../calculator.png" alt="calculator png" className="" />
-     <img src="../calendar.png" alt="calender" className="" />
-     <img src="../message-notif.png" alt="message-notif" className="" />
-     <img src="../Profile.png" alt="profile" className="" />
+     <img src="/Notification 1.png" alt="Myxellia Logo" className="" />
+     <img src="/calculator.png" alt="calculator png" className="" />
+     <img src="/calendar.png" alt="calender" className="" />
+     <img src="/messagenotif.png" alt="message-notif" className="" />
+     <img src="/Profile.png" alt="profile" className="" />
      </nav>
     </header>
   
